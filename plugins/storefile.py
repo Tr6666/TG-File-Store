e52915341ce31d5384a95c228d68ff3b29303d3d
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2021-12-26 13:16:56 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -34,10 +34,11 @@ async def storefile(c, m):
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
     url = f"https://telegram.dog/{bot.username}?start={base64_string}"
+    short_url = f"https://droplink.co/st?api=1aab74171e9891abd0ba799e3fd568c9598a79e1&url={url}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
-    
     text += f"<code>{url}</code>"
+    text += f"/n------------/nShort URL - <code>{short_url}</code>"
 
     # making buttons
     buttons = [[
@@ -91,6 +92,7 @@ async def storefile_channel(c, m):
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
     url = f"https://telegram.dog/{bot.username}?start={base64_string}"
+    short_url = f"https://droplink.co/st?api=1aab74171e9891abd0ba799e3fd568c9598a79e1&url={url}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 

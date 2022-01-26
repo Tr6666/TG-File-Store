582e13a60e42f16b1882d747cce1b88b92e2205d
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-26 23:21:20 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -4,9 +4,12 @@ from .commands import encode_string
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
+import aiohttp
 
+API_KEY = "1aab74171e9891abd0ba799e3fd568c9598a79e1"
 #################################### FOR PRIVATE ################################################
-@Client.on_message((filters.document|filters.video|filters.audio|filters.photo) & filters.incoming & ~filters.edited & ~filters.channel)
+@Client.on_message((
+                           filters.document | filters.video | filters.audio | filters.photo) & filters.incoming & ~filters.edited & ~filters.channel)
 async def storefile(c, m):
     if IS_PRIVATE:
         if m.from_user.id not in AUTH_USERS:
@@ -16,7 +19,7 @@ async def storefile(c, m):
     # text
     text = ""
     if not m.photo:
-  
+
         text += f"<b>{media.file_name}</b>\n\n" if media.file_name else ""
         text += f"📊 File Size: <b>{humanbytes(media.file_size)}</b>\n\n" if media.file_size else ""
         if not m.document:
@@ -24,7 +27,7 @@ async def storefile(c, m):
             if m.audio:
                 text += f"🎵 __Title:__ `{media.title}`\n\n" if media.title else ""
                 text += f"🎙 __Performer:__ `{media.performer}`\n\n" if media.performer else ""
-    
+
     # if databacase channel exist forwarding message to channel
     if DB_CHANNEL_ID:
         msg = await m.copy(int(DB_CHANNEL_ID))
@@ -38,13 +41,14 @@ async def storefile(c, m):
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{short_url}"
     text += f"<b>Url</b> - <code>{url}</code>"
-    text += f"\n\n<b>Droplink URL</b> - <code>{short_url}</code>"
+    dplink = await get_shortlink(url)
+    text += f"\n\n<b>Droplink URL</b> - <code>{dplink}</code>"
 
     # making buttons
     buttons = [[
         InlineKeyboardButton(text="Open Url 🔗", url=url),
         InlineKeyboardButton(text="Share Link 👤", url=share_url)
-        ],[
+    ], [
         InlineKeyboardButton(text="Delete 🗑", callback_data=f"delete+{msg.message_id}")
     ]]
 
@@ -54,9 +58,11 @@ async def storefile(c, m):
         reply_markup=InlineKeyboardMarkup(buttons)
     )
 
+
 #################################### FOR CHANNEL################################################
 
-@Client.on_message((filters.document|filters.video|filters.audio|filters.photo) & filters.incoming & filters.channel & ~filters.forwarded & ~filters.edited)
+@Client.on_message((
+                           filters.document | filters.video | filters.audio | filters.photo) & filters.incoming & filters.channel & ~filters.forwarded & ~filters.edited)
 async def storefile_channel(c, m):
     if IS_PRIVATE:
         if m.chat.id not in AUTH_USERS:
@@ -109,7 +115,7 @@ async def storefile_channel(c, m):
 def humanbytes(size):
     if not size:
         return ""
-    power = 2**10
+    power = 2 ** 10
     n = 0
     Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
@@ -124,8 +130,18 @@ def TimeFormatter(milliseconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = ((str(days) + " days, ") if days else "") + \
-        ((str(hours) + " hrs, ") if hours else "") + \
-        ((str(minutes) + " min, ") if minutes else "") + \
-        ((str(seconds) + " sec, ") if seconds else "") + \
-        ((str(milliseconds) + " millisec, ") if milliseconds else "")
+          ((str(hours) + " hrs, ") if hours else "") + \
+          ((str(minutes) + " min, ") if minutes else "") + \
+          ((str(seconds) + " sec, ") if seconds else "") + \
+          ((str(milliseconds) + " millisec, ") if milliseconds else "")
     return tmp[:-2]
+
+
+async def get_shortlink(link):
+    url = 'https://droplink.co/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]

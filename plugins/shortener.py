0fb10970f3e1e7e5db810e7a8dafd2328ffdf88e
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-29 19:05:30 +0530

    Update shortener.py

--- plugins/shortener.py
+++ plugins/shortener.py
@@ -1,17 +1,31 @@
 from pyrogram import Client, filters
 import aiohttp
+import re
 
 chat = [1166625664, 2132021818, 1383296549]
 
 
-@Client.on_message(filters.regex(r'https?://[^\s]+') & filters.private & filters.chat(chat))
-async def link_handler(bot, message):
-    link = message.matches[0].group(0)
+@Client.on_message(filters.regex(r'https?://[^\s]+') & filters.chat(chat))
+async def reply_text(c,m):
+    text = await m.reply_text("Processing")
     try:
-        short_link = await get_shortlink(link)
-        await message.reply(f'`{short_link}`', quote=True)
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
+        txt = await bulk_shortener(m.text)
+        await m.reply_text(txt)
+    except:
+        txt = await bulk_shortener(m.caption)
+        id = m.photo.file_id
+        await m.reply_photo(id, caption=f"**{txt}**")
+    await text.delete()
+
+
+async def bulk_shortener(message):
+    urls = re.findall(r'https?://[^\s]+', message)
+    for url in urls:
+        short_link = await get_shortlink(url)
+        if url in message:
+            message = message.replace(url, short_link)
+
+    return message
 
 
 async def get_shortlink(link):

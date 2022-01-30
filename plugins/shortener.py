6e020760f857e6e51cdd295e916a7c4130e21780
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-30 15:56:49 +0530

    Update shortener.py

--- plugins/shortener.py
+++ plugins/shortener.py
@@ -1,30 +1,49 @@
 import re
+import json
 import aiohttp
 from pyrogram import Client, filters
 import requests
 import emoji
+
 chat = [1166625664, 2132021818, 1383296549]
+
 
 # ##################################  droplink  #################################################################
 
 
 @Client.on_message(filters.command('dp') & filters.chat(chat))
 async def reply_text(c, m):
-    try:
-        text = await m.reply_text("`Processing...... May take some time`")
-        try:
-            txt = await bulk_shortener(m.reply_to_message.text)
-            r = f"**{txt}**\n\n**@T2links**"
-            await m.reply_text(r)
-        except:
-            txt = await bulk_shortener(m.reply_to_message.caption)
-            id = m.reply_to_message.photo.file_id
-            r = f"**{txt}**\n\n**@T2links**"
-            await m.reply_photo(id, caption=r)
-        await text.delete()
-    except:
-        await text.delete()
-        await m.reply_text("`Some Error Occurred, Use this command only to short your links to Droplink url`")
+
+    text = await m.reply_text("`Processing...... May take some time`")
+
+    if m.reply_to_message.reply_markup:
+        msg = ""
+        txt = m.reply_to_message.reply_markup
+        print(txt)
+        txt = json.loads(str(txt))
+        for t in txt["inline_keyboard"]:
+            for j in t:
+                texts = j["text"]
+                url = j["url"]
+                urls = await get_shortlink(url)
+                msg += f"**{texts} - [👉 Link 🔗]({urls})**\n\n"
+                print(msg)
+                print(urls)
+        print(msg)
+        await m.reply_text(msg)
+
+    elif m.reply_to_message.text:
+        txt = await bulk_shortener(m.reply_to_message.text)
+        r = f"**{txt}**\n\n**@T2links**"
+        await m.reply_text(r)
+    elif m.reply_to_message.media:
+        txt = await bulk_shortener(m.reply_to_message.caption)
+        id = m.reply_to_message.photo.file_id
+        r = f"**{txt}**\n\n**@T2links**"
+        await m.reply_photo(id, caption=r)
+
+    await text.delete()
+
 
 
 async def bulk_shortener(message):
@@ -63,19 +82,35 @@ async def get_shortlink(link):
 async def mdisk(c, m):
     try:
         text = await m.reply_text("`Processing...... May take some time`")
-        try:
+        if m.reply_to_message.reply_markup:
+            msg = ""
+            txt = m.reply_to_message.reply_markup
+            print(txt)
+            txt = json.loads(str(txt))
+            for t in txt["inline_keyboard"]:
+                for j in t:
+                    texts = j["text"]
+                    url = j["url"]
+                    urls = await get_mdisk(url)
+                    msg += f"**{texts} - [👉 Link 🔗]({urls})**\n\n"
+                    print(msg)
+                    print(urls)
+            print(msg)
+            await m.reply_text(msg)
+
+        elif m.reply_to_message.text:
             txt = await mdisk_bulk_shortener(m.reply_to_message.text)
             r = f"**{txt}**\n\n**@T2links**"
             await m.reply_text(r)
-        except:
+        elif m.reply_to_message.media:
             txt = await mdisk_bulk_shortener(m.reply_to_message.caption)
             id = m.reply_to_message.photo.file_id
             r = f"**{txt}**\n\n**@T2links**"
             await m.reply_photo(id, caption=r)
         await text.delete()
-    except:
+    except Exception as e:
         await text.delete()
-        await m.reply_text("`Some Error Occurred, Reply only to MDisk links`")
+        await m.reply_text(f"`Some Error Occurred, Reply only to MDisk links\n\n{e}`")
 
 
 async def mdisk_bulk_shortener(message):
@@ -108,26 +143,44 @@ async def get_mdisk(link):
     return shareLink["sharelink"]
 
 
-######################################## Mdisk and droplink #######################################################
+########################################Mdisk and droplink#######################################################
 
 
 @Client.on_message(filters.command('shorten') & filters.chat(chat))
 async def shorten(c, m):
     try:
         text = await m.reply_text("`Converting other MDisk links to your MDisk account... May take some time`")
-        try:
+        if m.reply_to_message.reply_markup:
+            msg = ""
+            txt = m.reply_to_message.reply_markup
+            print(txt)
+            txt = json.loads(str(txt))
+            for t in txt["inline_keyboard"]:
+                for j in t:
+                    texts = j["text"]
+                    url = j["url"]
+                    mdisk_url = await get_mdisk(url)
+                    urls = await get_shortlink(mdisk_url)
+                    msg += f"**{texts} - [👉 Link 🔗]({urls})**\n\n"
+                    print(msg)
+                    print(urls)
+            print(msg)
+            await m.reply_text(msg)
+        elif m.reply_to_message.text:
             txt = await mdisks_bulk_shortener(m.reply_to_message.text)
             await text.edit(
                 "Converted all links to your MDisk account. Now converting all your MDisk links to droplink url.....")
             txt = await bulk_shortener(txt)
             r = f"**{txt}**\n\n**@T2links**"
             await m.reply_text(r)
-        except:
+
+        elif m.reply_to_message.media:
             txt = await mdisks_bulk_shortener(m.reply_to_message.caption)
             txt = await bulk_shortener(txt)
             id = m.reply_to_message.photo.file_id
             r = f"**{txt}**\n\n**@T2links**"
             await m.reply_photo(id, caption=r)
+
         await text.delete()
     except:
         await text.delete()

d7bc60ee3b818845c5ad09f90f8fe65a2ceda77e
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-02-12 14:36:14 +0530

    Update others.py

--- plugins/others.py
+++ plugins/others.py
@@ -4,13 +4,14 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_message(filters.incoming & filters.channel & ~filters.edited)
 async def button_post(c, m):
-    chat = await c.get_chat(m.chat.id)
+    if m.text:
+        chat = await c.get_chat(m.chat.id)
 
-    buttons = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton('Join', url=chat.invite_link),
-            InlineKeyboardButton('More Updates', url='t.me/t2links')
-        ]
-    ])
+        buttons = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton('Join', url=chat.invite_link),
+                InlineKeyboardButton('More Updates', url='t.me/t2links')
+            ]
+        ])
 
-    await m.edit(m.text, entities=m.entities, reply_markup=buttons, disable_web_page_preview=True)
+        await m.edit(m.text, entities=m.entities, reply_markup=buttons, disable_web_page_preview=True)

6c5480da082237f6d2cb67186ddec4e5c3b7cfc5
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-02-15 23:19:12 +0530

    Update edit_button.py

--- plugins/edit_button.py
+++ plugins/edit_button.py
@@ -2,8 +2,24 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Client.on_message(filters.incoming & filters.channel & ~filters.edited & ~filters.chat(-1001753151448))
+CHAT = [-1001793534052, -1001400526156, -1001230546236, -1001286887151, -1001751988620, -1001172494089, -1001781675464,
+        -1001348118571, -1001242897508, -1001519791986, -1001730250578, -1001205374653, -1001726864577]
+
+dicts = []
+
+
+@Client.on_message(filters.incoming & filters.channel & ~filters.edited & filters.chat(CHAT))
 async def button_post(c, m):
+    
+    global dicts
+    message_id = m.message_id
+    chat_id = m.chat.id
+    dic = {
+        'message_id': message_id,
+        'chat_id': chat_id
+    }
+    dicts.append(dic)
+    
     if m.text:
         chat = await c.get_chat(m.chat.id)
 
@@ -15,3 +31,14 @@ async def button_post(c, m):
         ])
 
         await m.edit(m.text, entities=m.entities, reply_markup=buttons, disable_web_page_preview=True)
+        
+        
+@Client.on_message(filters.command('del'))
+async def auto_delete(c: Client, m: Message):
+    txt = await m.reply_text("Deleting")
+    global dicts
+    messages = dicts
+    for message in messages:
+        await c.delete_messages(chat_id=message["chat_id"], message_ids=int(message["message_id"]))
+    dicts = []
+    await txt.edit("Deleted")

78d215c83cad4b1171d2c8b8938434fbdfc3f760
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-04-30 18:33:35 +0530

    Update edit_button.py

--- plugins/edit_button.py
+++ plugins/edit_button.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types import Message
+import asyncio
 
 CHAT = [-1001793534052, -1001400526156, -1001230546236, -1001286887151, -1001751988620, -1001172494089, -1001781675464,
         -1001348118571, -1001242897508, -1001519791986, -1001730250578, -1001205374653, -1001726864577, -1001573047911]
@@ -10,7 +11,7 @@ dicts = []
 
 @Client.on_message(filters.incoming & filters.channel & ~filters.edited & filters.chat(CHAT))
 async def button_post(c, m):
-    
+    await asyncio.sleep(60)
     global dicts
     message_id = m.message_id
     chat_id = m.chat.id
@@ -35,6 +36,7 @@ async def button_post(c, m):
         
 @Client.on_message(filters.command('del'))
 async def auto_delete(c: Client, m: Message):
+    await asyncio.sleep(60)
     txt = await m.reply_text("Deleting")
     global dicts
     messages = dicts
@@ -46,7 +48,8 @@ async def auto_delete(c: Client, m: Message):
         
         
 @Client.on_message(filters.incoming & filters.channel & ~filters.edited & filters.chat(-1001667892071))
-async def main_channel_button_post(c, m):        
+async def main_channel_button_post(c, m):  
+        await asyncio.sleep(60)
         if m.text:
                         
           chat = await c.get_chat(m.chat.id)
